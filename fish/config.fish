9a17db2fdef05afaed9bd92ee881f7413c82ac74
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2023-03-08 09:46:04 +0300

    (fish) use nvim as manpager

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,5 @@ export PATH="$HOME/.cargo/bin:$PATH"
 set -gx GOPATH "$HOME/go"
 set -gx PATH "$GOPATH/bin:$PATH"
 set -gx XDG_DATA_HOME "$HOME/.config"
+set -gx MANPAGER 'nvim +Man!'
 fish_vi_key_bindings
