6b386c6e366cad9aea8af38eaa0f8180c5421f86
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2024-02-21 15:54:40 +0300

    (nvim) add dadbod

--- nvim/init.lua
+++ nvim/init.lua
@@ -601,6 +601,23 @@ local plugins = {
 		},
 		cmd = { "Presenting" },
 	},
+
+	-- DB
+	{ "tpope/vim-dadbod" },
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function() vim.g.db_ui_use_nerd_fonts = 1 end,
+	},
 }
 
 require("lazy").setup(plugins, {
